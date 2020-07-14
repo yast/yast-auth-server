2d6c98cc39ad26d5188d32e498b3a5f6b251e55b
José Iván López González <jlopez@suse.com>
2020-07-14 12:58:44 +0100

    Add unit tests

--- test/krb_test.rb
+++ test/krb_test.rb
@@ -75,4 +75,108 @@ describe MITKerberos do
 '
     expect(MITKerberos.gen_kdc_conf('EXAMPLE.COM', 'cn=kdc', 'cn=adm', 'cn=container', '/pass', 'dir.example.net')).to eq(match)
   end
+
+  shared_context "kdb5_ldap_util mock" do
+    before do
+      allow(File).to receive(:exist?).and_call_original
+      allow(File).to receive(:exist?).with("/usr/lib/mit/sbin/kdb5_ldap_util").and_return(old_path)
+      allow(File).to receive(:exist?).with("/usr/sbin/kdb5_ldap_util").and_return(!old_path)
+
+      allow(File).to receive(:chmod)
+
+      allow(Open3).to receive(:popen2e).and_return([stdin, stdouterr, waiter])
+    end
+
+    let(:stdin) { instance_double(IO, puts: true, close: true) }
+
+    let(:stdouterr) { instance_double(IO, readlines: outerr) }
+
+    let(:waiter) { instance_double(Process::Waiter, value: status) }
+
+    let(:status) { instance_double(Process::Status, exitstatus: exitstatus) }
+
+    let(:outerr) { [] }
+
+    let(:exitstatus) { 0 }
+
+    let(:old_path) { false }
+  end
+
+  shared_examples "kdb5_ldap_util" do |method, *args|
+    context "when the kdb5_ldap_util is found in /usr/sbin" do
+      let(:old_path) { false }
+
+      it "calls kdb5_ldap_util from /usr/sbin" do
+        expect(Open3).to receive(:popen2e).with("/usr/sbin/kdb5_ldap_util", any_args)
+
+        MITKerberos.send(method, *args)
+      end
+    end
+
+    context "when the kdb5_ldap_util is not found in /usr/sbin" do
+      let(:old_path) { true }
+
+      it "calls kdb5_ldap_util from /usr/lib/mit/sbin" do
+        expect(Open3).to receive(:popen2e).with("/usr/lib/mit/sbin/kdb5_ldap_util", any_args)
+
+        MITKerberos.send(method, *args)
+      end
+    end
+
+    context "on success" do
+      let(:outerr) { ["message1", "error1"] }
+
+      let(:exitstatus) { 0 }
+
+      it "returns stdouterr and true" do
+        result = MITKerberos.send(method, *args)
+
+        expect(result).to eq(["message1\\nerror1", true])
+      end
+    end
+
+    context "on failure" do
+      let(:outerr) { ["message1", "error1"] }
+
+      let(:exitstatus) { 1 }
+
+      it "returns stdouterr and false" do
+        result = MITKerberos.send(method, *args)
+
+        expect(result).to eq(["message1\\nerror1", false])
+      end
+    end
+  end
+
+  describe ".save_password_into_file" do
+    include_context "kdb5_ldap_util mock"
+
+    it "calls kdb5_ldap_util with correct arguments" do
+      expect(Open3).to receive(:popen2e)
+        .with(/kdb5_ldap_util/, "stashsrvpw", "-f", "path/to/file", "-w", "pass", "example")
+
+      MITKerberos.save_password_into_file("example", "pass", "path/to/file")
+    end
+
+    include_examples "kdb5_ldap_util", :save_password_into_file, "example", "pass", "path/to/file"
+  end
+
+  describe ".init_dir" do
+    include_context "kdb5_ldap_util mock"
+
+    it "calls kdb5_ldap_util with correct arguments" do
+      expect(Open3).to receive(:popen2e)
+        .with(/kdb5_ldap_util/,
+          "-H", "ldaps://addr",
+          "-D", "dn",
+          "-w", "a_pass",
+          "create", "-r", "name",
+          "-subtrees", "c_dn",
+          "-s", "-P", "m_pass")
+
+      MITKerberos.init_dir("addr", "dn", "a_pass", "name", "c_dn", "m_pass")
+    end
+
+    include_examples "kdb5_ldap_util", :init_dir, "addr", "dn", "a_pass", "name", "c_dn", "m_pass"
+  end
 end
